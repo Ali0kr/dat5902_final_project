d39f751a7f60f1dc55780a29e8bf4cb5aaaece5f
Ali0kr <114663507+Ali0kr@users.noreply.github.com>
2025-01-04 15:24:02 +0000

    created second unit test

--- unit_tests.py
+++ unit_tests.py
@@ -3,10 +3,20 @@ import unittest
 import os
 
 #Functions to be tested
+#Checks if the correct file exists within the repository
 def checkInputFile():
     current_dir = os.getcwd()
-    print(current_dir)
     if os.path.exists(current_dir+'/testcenterdata.xlsx'):
+        return True
+    else:
+        return False
+
+#Checks if the file is the correct size - ensures data is the same
+def checkFileSize():
+    current_dir = os.getcwd()
+    file_dir = current_dir+'/testcenterdata.xlsx'
+    print(os.stat(file_dir).st_size)
+    if os.stat(file_dir).st_size == 3958123:
         return True
     else:
         return False
@@ -17,6 +27,10 @@ class TestFunction(unittest.TestCase):
     
     def test_filename(self):
         result = checkInputFile()
+        self.assertEqual(result,True)
+        
+    def test_filesize(self):
+        result = checkFileSize()
         self.assertEqual(result,True)
 
 #Mandatory to be run
