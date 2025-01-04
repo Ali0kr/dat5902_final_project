edb283347b545f87c23f1673c126dfd1e9f49662
Ali0kr <114663507+Ali0kr@users.noreply.github.com>
2025-01-04 14:39:38 +0000

    created data check unit test

--- unit_tests.py
+++ unit_tests.py
@@ -1,15 +1,22 @@
 #Import
 import unittest
+import os
 
 #Functions to be tested
-def checkInputFile(filename):
-    if filename == "":
+def checkInputFile():
+    curr_dir = os.getcwd()
+    if os.curr_dir.exists("testcenterdata.xlsx"):
         return True
     else:
         return False
 
 #Testing section
 
+class TestFunction(unittest.TestCase):
+    
+    def test_filename(self):
+        result = checkInputFile()
+        self.assertEqual(result,True)
 
 #Mandatory to be run
 if __name__ == '__main__':
