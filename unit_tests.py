24e9bf896c05e7155a3277896235f03dc43d2b2b
Ali0kr <114663507+Ali0kr@users.noreply.github.com>
2025-01-04 14:17:00 +0000

    begun unit testing

---
+++ unit_tests.py
@@ -0,0 +1,16 @@
+#Import
+import unittest
+
+#Functions to be tested
+def checkInputFile(filename):
+    if filename == "":
+        return True
+    else:
+        return False
+
+#Testing section
+
+
+#Mandatory to be run
+if __name__ == '__main__':
+    unittest.main()
