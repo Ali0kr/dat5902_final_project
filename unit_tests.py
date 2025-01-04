31ea9494e1d02180d4fa2c23451066f42a189cc2
Ali0kr <114663507+Ali0kr@users.noreply.github.com>
2025-01-04 15:25:50 +0000

    fixed unit test - works correctly

--- unit_tests.py
+++ unit_tests.py
@@ -15,8 +15,7 @@ def checkInputFile():
 def checkFileSize():
     current_dir = os.getcwd()
     file_dir = current_dir+'/testcenterdata.xlsx'
-    print(os.stat(file_dir).st_size)
-    if os.stat(file_dir).st_size == 3958122:
+    if os.stat(file_dir).st_size == 3958123:
         return True
     else:
         return False
