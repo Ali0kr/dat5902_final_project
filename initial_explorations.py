a5569779e8cb5b918504377674d718b819c29560
Ali0kr <114663507+Ali0kr@users.noreply.github.com>
2025-01-08 12:54:04 +0000

    Implemented updated readInData function to also clean data

--- initial_explorations.py
+++ initial_explorations.py
@@ -8,7 +8,10 @@ def readInData (fileName,sheet):
     current_dir = os.getcwd()
     file_dir = current_dir+fileName
     data = pd.read_excel(file_dir,sheet) #Reads in the specified sheet index from the data file
-    return data
+    clean = data.iloc[12::9,[0,3,7,11]] #Takes only the totals from each location and only the male, female and total pass %s 
+    clean.columns = ['Location','Male%','Female%','Total%']
+    clean = clean.reset_index(drop=True) #Tidies up dataframe by correcting index values
+    return clean
 
 #Creating dataframes 
 fileName = '/testcenterdata.xlsx'
